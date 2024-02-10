fd47bf28362fac511ecb3a11c5811bde1291e641
ShivaniNair2003 <111459651+ShivaniNair2003@users.noreply.github.com>
2024-02-10 13:11:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     var generateBtn = document.getElementById('generateBtn');
     var resultElement = document.getElementById('result');
+
     generateBtn.addEventListener('click', function () {
-        var randomNumber = Math.floor(Math.random() * 100) + 1;
+        var randomNumber = Math.floor(Math.random() * (1000 - (-50) + 1)) + (-50);
+        var randomColor = getRandomColor();
+
         resultElement.textContent = 'Random Number: ' + randomNumber;
+        resultElement.style.color = randomColor;
+        generateBtn.style.backgroundColor = randomColor;
     });
+
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
 });
